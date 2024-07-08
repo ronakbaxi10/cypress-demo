2bda4880519c3cc40db17d922fe374ee08826078
Matthew Hayhurst <matthewjhayhurst@gmail.com>
2024-07-08 14:02:07 +0100

    [MJH] The master database should now work for ANY column

--- cypress/pageObjects/masterDatabase/masterDatabasePage.js
+++ cypress/pageObjects/masterDatabase/masterDatabasePage.js
@@ -27,105 +27,76 @@ get  hasAlarmsRow1ResultIsChecked() {
 
 enterFilterValue(filterName, filterValue){
   let valueToEnter=filterValue+'{enter}'
-  switch (filterName) {
-    case 'Tag Name':
-      this.getColumnNumberAndEnterFilterText(filterName,valueToEnter)
-      //Check the correct filter is shown at the bottom
-      this.checkSelectedFiltersValue(filterValue)
-      break; 
-    case 'Project Name':
-      this.getColumnNumberAndEnterFilterText(filterName,valueToEnter)
-      //Check the correct filter is shown at the bottom
-      this.checkSelectedFiltersValue(filterValue)
-      break; 
-    case 'Tag Status':
-      this.getColumnNumberAndEnterFilterText(filterName,valueToEnter)
-      //Check the correct filter is shown at the bottom
-      this.checkSelectedFiltersValue(filterValue)
-      break; 
-    case 'Template Name':
-      this.getColumnNumberAndEnterFilterText(filterName,valueToEnter)
-      //Check the correct filter is shown at the bottom
-      this.checkSelectedFiltersValue(filterValue)
-      break; 
-    case 'Has Alarms':
-      this.getColumnNumberAndEnterFilterText(filterName,valueToEnter)
-      //Check the correct filter is shown at the bottom
-      this.checkSelectedFiltersValue(filterValue)
-      break; 
-    default:
-      cy.get('body').then(() => {
-        throw new Error('ERROR! No items in the switch statement match the passed in value of: ' + filterName +'.');
-      }) 
-  }  
+    this.getColumnNumberAndEnterFilterText(filterName,valueToEnter);
+    //Check the correct filter is shown at the bottom
+    this.checkSelectedFiltersValue(filterValue);
   //The filter selected checkbox should now be displayed:
-  this.filterSelectedCheckBox.should('be.visible')
+  this.filterSelectedCheckBox.should('be.visible');
 }
 
 getColumnNumberAndEnterFilterText(filterTitle, valueToEnter){
+  //Find the column number by searching for the passed in Title and enter the value. Error if no matching column title found
+  let matchingTitle = false;
   this.filterTitlesRowArray.each(($ele, index) => {
-    if ($ele.text().includes(filterTitle)) {
-      cy.log("************"+filterTitle+" = Column "+((parseInt(index))-1) +"***************")
-    this.filterInputFieldsArray.eq(((parseInt(index))-1))
-        .should('be.visible')
-        .click()
-        .type(valueToEnter);
-    }
-  })
+      if ($ele.text().includes(filterTitle)) {
+        cy.log("************"+filterTitle+" = Column "+((parseInt(index))-1) +"***************")
+        this.filterInputFieldsArray.eq(((parseInt(index))-1))
+          .should('be.visible')
+          .click()
+          .type(valueToEnter)
+          .then(() => {matchingTitle = true;})
+      }
+    })
+    .then(() => {
+      if (matchingTitle == false) {
+        cy.get('body').then(() => {
+          throw new Error('ERROR! There are no column titles that match the passed in value of: ' + filterTitle +'.');
+        }) 
+      }
+    })
 }
 
-checkRow1ColumnFieldContainsValue(columnName, expectedValue){
-  switch (columnName) {
-    case 'Tag Name':
-      this.getColumnNumberAndCheckValue(columnName, expectedValue);
-      break; 
-    case 'Project Name':
-      this.getColumnNumberAndCheckValue(columnName, expectedValue);
-      break; 
-    case 'Tag Status':
-      this.getColumnNumberAndCheckValue(columnName, expectedValue);
-      break; 
-    case 'Template Name':
-      this.getColumnNumberAndCheckValue(columnName, expectedValue);
-      break; 
-    case 'Has Alarms':
-      this.getColumnNumberAndCheckValue(columnName, expectedValue);
-    break; 
-    default:
-      cy.get('body').then(() => {
-        throw new Error('ERROR! No items in the switch statement match the passed in value of: ' + columnName +'.');
-      }) 
-  }  
-}
-
-getColumnNumberAndCheckValue(filterTitle, expectedValue){
+checkRow1ColumnFieldContainsValue(filterTitle, expectedValue){
+  let matchingTitle = false;
   this.filterTitlesRowArray.each(($ele, index) => {
     if ($ele.text().includes(filterTitle)) {
       cy.log("************"+filterTitle+" = Column "+((parseInt(index))-1) +"***************")
       if(expectedValue != 'Checked' && expectedValue != "Unchecked") {
-      this.row1ResultFieldsArray.eq(((parseInt(index))))
-      .should('be.visible')
-      .should('contain',expectedValue);
+        this.row1ResultFieldsArray.eq(((parseInt(index))))
+        .should('be.visible')
+        .should('contain',expectedValue)
+        .then(() => {matchingTitle = true;})
       }
-  else {
-    switch (expectedValue) {
-      case 'Checked':
-        this.row1ResultFieldsArray.eq(((parseInt(index))))
-        .should('be.visible')
-        .find('.fa-solid').should('exist');
-        break; 
-      case 'Unchecked':
-        this.row1ResultFieldsArray.eq(((parseInt(index))))
-        .should('be.visible')
-        .find('.fa-solid').should('not.exist');
-        break; 
-      default:
-      cy.get('body').then(() => {
-        throw new Error('ERROR! No items in the switch statement match the passed in value of: ' + expectedValue +'.');
-      }) 
-  }
-  }
-}})
+      else {
+        switch (expectedValue) {
+          case 'Checked':
+            this.row1ResultFieldsArray.eq(((parseInt(index))))
+            .should('be.visible')
+            .find('.fa-solid').should('exist')
+            .then(() => {matchingTitle = true;})
+            break; 
+          case 'Unchecked':
+            this.row1ResultFieldsArray.eq(((parseInt(index))))
+            .should('be.visible')
+            .find('.fa-solid').should('not.exist')
+            .then(() => {matchingTitle = true;})
+            break; 
+          default:
+          cy.get('body').then(() => {
+            throw new Error('ERROR! No items in the switch statement match the passed in value of: ' + expectedValue +'.');
+          }) 
+        }
+      }
+    } 
+  })
+    .then(() => {
+      if (matchingTitle == false) {
+        cy.get('body').then(() => {
+          throw new Error('ERROR! There are no column titles that match the passed in value of: ' + filterTitle +'.');
+        }) 
+      }
+    })
 }
 }
+
 export default new masterDatabasePage();
